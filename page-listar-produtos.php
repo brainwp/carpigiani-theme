4afa7156d00922c70c850f9d1a39cd9ffb01fca8
Jony Fernando Schulz <jonymidia@gmail.com>
2014-05-07 08:29:48 -0300

    Criado a page-listar-produtos e a get template dela

---
+++ page-listar-produtos.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * 
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package carpigiani-theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<section class="slider-content-single">
+					<?php get_template_part( 'content', 'produtos' ); ?>
+				</section><!-- #carousel .slider-content-single -->
+
+			<?php endwhile; // end of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php //get_sidebar(); ?>
+<?php get_footer(); ?>
