049fcca288e61aadd1b15770abc0150d2bc6a9c8
everaldomatias <matiaseveraldo@gmail.com>
2014-05-07 13:34:06 -0300

    Ajustes CSS da Page e tamanho da fonte da Single Fixes #44 Fixes #45

--- content-page.php
+++ content-page.php
@@ -5,16 +5,17 @@
  * @package carpigiani-theme
  */
 ?>
-
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'content-page' ); ?>>
-	<h1 class="entry-title"><?php the_title(); ?></h1>
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'carpigiani-theme' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-</article><!-- #post-## -->
+<div class="wrap">
+	<article id="post-<?php the_ID(); ?>" <?php post_class( 'content-page' ); ?>>
+		<h1 class="entry-title"><?php the_title(); ?></h1>
+		<div class="entry-content">
+			<?php the_content(); ?>
+			<?php
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . __( 'Pages:', 'carpigiani-theme' ),
+					'after'  => '</div>',
+				) );
+			?>
+		</div><!-- .entry-content -->
+	</article><!-- #post-## -->
+</div><!-- .wrap -->
