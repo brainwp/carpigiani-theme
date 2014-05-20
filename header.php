dbc968210594bd604e5627fc0d79d952919e0269
Jony Fernando Schulz <jonymidia@gmail.com>
2014-05-20 17:55:31 -0300

    Adicionado menu responsive

--- header.php
+++ header.php
@@ -17,7 +17,6 @@
 
 <link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.png" type="image/x-icon">
 
-
 <link href='http://fonts.googleapis.com/css?family=Montserrat+Alternates:400,700' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,700italic,400,600,700' rel='stylesheet' type='text/css'>
 
@@ -33,6 +32,10 @@
 	$( '#carousel-elast' ).elastislide();	
 </script> -->
 
+<link rel="stylesheet" href="<?php bloginfo( 'template_url' ) ?>/rmm-css/responsivemobilemenu.css" type="text/css" media="screen" />
+<script type="text/javascript" src="<?php bloginfo( 'template_url' ) ?>/js/responsivemobilemenu.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+
 <?php wp_head(); ?>
 </head>
 
@@ -43,14 +46,41 @@
 
 		<section class="track-header">
 			<div class="wrap">
+				<div class="rmm">
+		            <ul>
+						<?php
+							$defaults = array(
+								'theme_location'  => '',
+								'menu'            => '',
+								'container'       => 'div',
+								'container_class' => '',
+								'container_id'    => '',
+								'menu_class'      => 'menu',
+								'menu_id'         => '',
+								'echo'            => true,
+								'fallback_cb'     => 'wp_page_menu',
+								'before'          => '',
+								'after'           => '',
+								'link_before'     => '',
+								'link_after'      => '',
+								'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+								'depth'           => 0,
+								'walker'          => ''
+							);
+							wp_nav_menu( $defaults );
+						?>		                
+		            </ul>
+		        </div>
 
 				<div class="logo">
 					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 				</div><!-- .logo -->
 
+
 				<div class="site-branding">
-					<nav id="site-navigation" class="main-navigation" role="navigation">
-						<button class="menu-toggle"><?php _e( 'Primary Menu', 'carpigiani-theme' ); ?></button>
+					<!-- <nav id="site-navigation" class="main-navigation" role="navigation"> -->
+					<nav id="" class="main-navigation" role="navigation">
+						<!-- <button class="menu-toggle"><?php //_e( 'Primary Menu', 'carpigiani-theme' ); ?></button> -->
 						<a class="skip-link screen-reader-text" href="#content">
 							<?php _e( 'Skip to content', 'carpigiani-theme' ); ?>
 						</a>
@@ -80,6 +110,7 @@
 						?>
 						<?php //wp_nav_menu( array( 'theme_location' => 'primary', 'items_wrap' => '<ul><li class="item-menu">%3$s</li></ul>' ) ); ?>
 					</nav>
+
 				</div><!-- #site-navigation -->
 
 				<form id="search-form" role="search" method="get" id="searchform" class="search-form" action="<?php echo home_url( '/' ); ?>">
