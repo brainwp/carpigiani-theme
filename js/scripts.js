125553e56fd18766abba7660b1bdd6248e66082c
matheuspetroni <contato@matheusgimenez.com.br>
2014-05-21 14:58:17 -0300

    php do slider produtos

--- js/scripts.js
+++ js/scripts.js
@@ -50,9 +50,9 @@ jQuery(function($) {
 			}
 		}
 	});
-	$('#foo2').carouFredSel({
-		prev: '#prev-outros',
-		next: '#next-outros',
+	$('#cat-prod-container').carouFredSel({
+		prev: '#prev-slider',
+		next: '#next-slider',
 		responsive: true,
 		width: '100%',
 		scroll: {
@@ -60,7 +60,7 @@ jQuery(function($) {
 			pauseOnHover: true,
 		},
 		items: {
-			width: 400,
+			width: 980,
 			visible: {
 				min: 1,
 				max: 1
