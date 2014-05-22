6e2e2fa8494dd28ebde9297e99f5905f7ecab7f1
Jony Fernando Schulz <jonymidia@gmail.com>
2014-05-22 17:14:40 -0300

    Estilizando e aplicando php em taxonomy

--- taxonomy.php
+++ taxonomy.php
@@ -9,96 +9,55 @@
 
 get_header(); ?>
 
+<style>
+	.flex-container {
+		max-width: 980px;
+		margin: 0 auto;
+		display: -webkit-flex;
+		display: -moz-flex;
+		display: -ms-flex;
+		display: -o-flex;
+    	display: flex;
+    	flex-wrap: wrap; /* Quebra de linha */
+    	/* -webkit-flex-flow: column;
+    	flex-flow: column; */
+	}
+	.flex-container div {
+		background:#CCC;
+    	margin:10px;
+    	width:176px;
+    	height:176px;
+
+    	-webkit-flex:1 1 auto;
+    	-moz-flex:1 1 auto;
+    	-ms-flex:1 1 auto;
+    	-o-flex:1 1 auto;
+    	flex:1 1 auto;
+	}
+</style>
+
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 			<section class="content-page">
 				<div class="wrap">
 
-					<?php if ( have_posts() ) : ?>
-
-						<header class="page-header">
-							<h1 class="page-title">
-								<?php
-									if ( is_category() ) :
-										single_cat_title();
-
-									elseif ( is_tag() ) :
-										single_tag_title();
-
-									elseif ( is_author() ) :
-										printf( __( 'Author: %s', 'carpigiani-theme' ), '<span class="vcard">' . get_the_author() . '</span>' );
-
-									elseif ( is_day() ) :
-										printf( __( 'Day: %s', 'carpigiani-theme' ), '<span>' . get_the_date() . '</span>' );
-
-									elseif ( is_month() ) :
-										printf( __( 'Month: %s', 'carpigiani-theme' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'carpigiani-theme' ) ) . '</span>' );
-
-									elseif ( is_year() ) :
-										printf( __( 'Year: %s', 'carpigiani-theme' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'carpigiani-theme' ) ) . '</span>' );
-
-									elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
-										_e( 'Asides', 'carpigiani-theme' );
-
-									elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
-										_e( 'Galleries', 'carpigiani-theme');
-
-									elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
-										_e( 'Images', 'carpigiani-theme');
-
-									elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
-										_e( 'Videos', 'carpigiani-theme' );
-
-									elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
-										_e( 'Quotes', 'carpigiani-theme' );
-
-									elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
-										_e( 'Links', 'carpigiani-theme' );
-
-									elseif ( is_tax( 'post_format', 'post-format-status' ) ) :
-										_e( 'Statuses', 'carpigiani-theme' );
-
-									elseif ( is_tax( 'post_format', 'post-format-audio' ) ) :
-										_e( 'Audios', 'carpigiani-theme' );
-
-									elseif ( is_tax( 'post_format', 'post-format-chat' ) ) :
-										_e( 'Chats', 'carpigiani-theme' );
-
-									else :
-										_e( 'Archives', 'carpigiani-theme' );
-
-									endif;
-								?>
-							</h1>
-							<?php
-								// Show an optional term description.
-								$term_description = term_description();
-								if ( ! empty( $term_description ) ) :
-									printf( '<div class="taxonomy-description">%s</div>', $term_description );
-								endif;
-							?>
-						</header><!-- .page-header -->
-
-						<?php /* Start the Loop */ ?>
-						<?php while ( have_posts() ) : the_post(); ?>
-
-							<?php
-								/* Include the Post-Format-specific template for the content.
-								 * If you want to override this in a child theme, then include a file
-								 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-								 */
-								get_template_part( 'content', get_post_format() );
-							?>
-
-						<?php endwhile; ?>
-
-						<?php carpigiani_theme_paging_nav(); ?>
-
-					<?php else : ?>
-
-						<?php get_template_part( 'content', 'none' ); ?>
-
-					<?php endif; ?>
+					<ul class="flex-container">
+						<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+						<div><?php the_title(); ?></div>
+						<!-- <div></div>
+						
+						<div></div>
+						<div></div>
+						<div></div>
+						<div></div>
+						<div></div>
+						<div></div>
+						<div></div>
+						<div></div> -->
+						<?php endwhile; else: ?>
+						<p><?php _e('Desculpe, nenhum post encontrado com seus critérios.'); ?></p>
+						<?php endif; ?>
+					</ul><!-- .flex-container -->
 
 				</div><!-- .wrap -->
 			</section><!-- .content-page -->
