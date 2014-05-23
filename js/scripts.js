eb03115b868497aceec92cc9d9766c9682c23f66
matheuspetroni <contato@matheusgimenez.com.br>
2014-05-23 14:57:54 -0300

    slider

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,60 @@
-jQuery(function($) { 
+jQuery(function($) {
+
+	$.ajaxSetup({cache:false});
+	$(".cat-hover").on('click',function(){
+		var post_link = $(this).attr("rel");
+		var slug = $(this).attr('data-class-slug');
+		var id = $(this).attr('data-id');
+		//alert(post_id);
+		//$("#single-home-container").html("<div class='box'><span class='loader5'></span></div>");
+		$("#foo7").html("<div class='box-loader'><span class='clock'></span></div>");
+		$.get(post_link, function( data ) {
+			$("#foo7").trigger("pause");
+			$("#foo7").html(data);
+			$("#foo7").trigger("play", true);
+			$('#foo7').carouFredSel({
+				prev: '#prev-slider',
+				next: '#next-slider',
+				responsive: true,
+				width: '100%',
+				scroll: {
+					items: 4,
+					pauseOnHover: true
+				},
+				items: {
+					width: 250,
+					visible: {
+						min: 4,
+						max: 4
+					}
+				}
+			});
+		});
+		$('#produtos-row').removeClass('cat-soft cat-restaurante cat-chocolate-e-creme cat-artesanal');
+		$('#produtos-row').addClass('cat-'+slug);
+		$('.cat-hover').each(function(){
+			$(this).removeClass('active');
+		})
+		$(this).addClass('active');
+		return false;
+	});
+	$('#foo7').carouFredSel({
+		prev: '#prev-slider',
+		next: '#next-slider',
+		responsive: true,
+		width: '100%',
+		scroll: {
+			items: 1,
+			pauseOnHover: true
+		},
+		items: {
+			width: 250,
+			visible: {
+				min: 1,
+				max: 4
+			}
+		}
+	});
 	$('#foo5').carouFredSel({
 		prev: '#prev-slider',
 		next: '#next-slider',
@@ -50,9 +106,9 @@ jQuery(function($) {
 			}
 		}
 	});
-	$('#cat-prod-container').carouFredSel({
-		prev: '#prev-slider',
-		next: '#next-slider',
+	$('#foo2').carouFredSel({
+		prev: '#prev-outros',
+		next: '#next-outros',
 		responsive: true,
 		width: '100%',
 		scroll: {
@@ -60,7 +116,7 @@ jQuery(function($) {
 			pauseOnHover: true,
 		},
 		items: {
-			width: 980,
+			width: 400,
 			visible: {
 				min: 1,
 				max: 1
