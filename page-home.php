4650503d4b407e031855778dd8b033658bfe7d43
matheus <contato@matheusgimenez.com.br>
2014-11-05 18:52:12 -0200

    adicionando campo de imagem acf

--- page-home.php
+++ page-home.php
@@ -18,7 +18,13 @@ get_header(); ?>
 							Veja <?php the_title(); ?> &gt;&gt;
 						</a><!-- .btn -->		
 					</div><!-- .excerpt -->
-					<div class="image"><?php the_post_thumbnail( 'slider-home' ); ?></div><!-- .image -->
+					<?php $img = get_post_meta( get_the_ID(), 'image_slider_home', true ); ?>
+					<div class="image">
+						<?php if($img !== false && !empty($img)): ?>
+						   <?php $img = wp_get_attachment_image_src( $img, 'image_slider_home', false ); ?>
+						   <img src="<?php echo $img[0]; ?>" width="<?php echo $img[1]; ?>" height="<?php echo $img[2]; ?>">
+						<?php endif; ?>
+					</div><!-- .image -->
 				</li><!-- .item -->
 			<?php endwhile;  wp_reset_query(); ?>
 
